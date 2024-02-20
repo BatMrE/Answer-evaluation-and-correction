13bb835f813d3dde4776f7e5947be0a4321e584b
A R Khan <rafeyk10@gmail.com>
2024-02-19 23:52:58 -0500

    Initial main

---
+++ main.py
@@ -0,0 +1,65 @@
+from operations import *
+from data import essay_text_list
+
+for essay_text in essay_text_list:
+
+    gram_count = 0
+    # text = "He walk to the store."
+    errors = find_grammar_errors(essay_text)
+    print("Grammar errors found:")
+    for error in errors:
+        gram_count+=1
+        # print("-", error)
+    print("gram_count ", gram_count)
+
+
+    lexical_diversity_score = calculate_lexical_diversity(essay_text)
+    print("Lexical Diversity Score:", lexical_diversity_score)
+
+
+    sections = get_sentences(essay_text)
+    phraseology_scores2 = []
+    for i in range(len(sections)-1):
+        score2 = calculate_phraseology_score(sections[i], sections[i+1])
+        phraseology_scores2.append(score2)
+
+    # print("Phraseology Scores for Sections:")
+    # for i, score in enumerate(phraseology_scores2, 1):
+    #     print(f"Section {i}: {score2}")
+
+    print("Average : ", sum(phraseology_scores2)/len(phraseology_scores2))
+
+
+
+    phraseology_scores = []
+    for i in range(len(sections)-1):
+        score = calculate_phrase_relation_score(sections[i], sections[i+1])
+        phraseology_scores.append(score)
+
+    # print("Phraseology Scores for Sections:")
+    # for i, score in enumerate(phraseology_scores, 1):
+    #     print(f"Section {i}: {score}")
+    print("Average Phraseology : ", sum(phraseology_scores)/len(phraseology_scores))
+
+
+    print("Vocabulary Score:" , calculate_vocabulary_score2(essay_text))
+
+
+    score = calculate_vocabulary_score(essay_text)
+    print("Vocabulary score:", score)
+
+
+    list_cohesion_score = []
+    sentences = sent_tokenize(essay_text)
+    for sentence in sentences:
+        cohesion_score = calculate_cohesion_score(sentence)
+        list_cohesion_score.append(cohesion_score)
+        # print("Cohesion score for senten ce '{}': {}".format(sentence, cohesion_score))
+
+    print("Average cohesion_score : ", sum(list_cohesion_score)/len(list_cohesion_score))
+
+
+    score = conventions_score(essay_text)
+    print("Conventions Score:", score)
+
+    print("______________________________________________________________ ")
