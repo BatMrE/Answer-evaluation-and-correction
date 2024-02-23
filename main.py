6b98d906f8d8fa0fe3f6b490be8ef97a63f828e5
A R Khan <rafeyk10@gmail.com>
2024-02-22 23:25:19 -0500

    code update

--- main.py
+++ main.py
@@ -1,70 +1,41 @@
 from operations import *
 from data import essay_text_list
 
-for essay_text in essay_text_list:
-
-    gram_count = 0
+def analyze_essay(essay_text):
+    # Find grammar errors
     errors = find_grammar_errors(essay_text)
-    print("Grammar errors found:")
-    for error in errors:
-        gram_count+=1
-        # print("-", error)
-    print("gram_count ", gram_count)
+    gram_count = len(errors)
+    print("Grammar errors found:", gram_count)
 
-
+    # Calculate lexical diversity score
     lexical_diversity_score = calculate_lexical_diversity(essay_text)
     print("Lexical Diversity Score:", lexical_diversity_score)
 
-
+    # Calculate phraseology scores
     sections = get_sentences(essay_text)
-    phraseology_scores2 = []
-    for i in range(len(sections)-1):
-        score2 = calculate_phraseology_score(sections[i], sections[i+1])
-        phraseology_scores2.append(score2)
+    phraseology_scores = [calculate_phrase_relation_score(sections[i], sections[i+1]) for i in range(len(sections)-1)]
+    phraseology = sum(phraseology_scores) / len(phraseology_scores) if phraseology_scores else 0
+    print("phraseology Score matrix:", phraseology)
 
-    # print("Phraseology Scores for Sections:")
-    # for i, score in enumerate(phraseology_scores2, 1):
-    #     print(f"Section {i}: {score2}")
-    phraseology2 = 0
-    if(len(phraseology_scores2)>0):
-        phraseology2 = sum(phraseology_scores2)/len(phraseology_scores2)
-    
-    # print("Average : ", sum(phraseology_scores2)/len(phraseology_scores2))
+    phraseology_scores2 = [calculate_phraseology_score(sections[i], sections[i+1]) for i in range(len(sections)-1)]
+    phraseology2 = sum(phraseology_scores2) / len(phraseology_scores2) if phraseology_scores2 else 0
+    print("phraseology Score:", phraseology2)
 
+    # Calculate vocabulary score
+    vocab_score = calculate_vocabulary_score2(essay_text)
+    print("Vocabulary Score:", vocab_score)
 
+    # Calculate cohesion score
+    list_cohesion_score = [calculate_cohesion_score(sentence) for sentence in sent_tokenize(essay_text)]
+    avg_cohesion_score = sum(list_cohesion_score) / len(list_cohesion_score)
+    print("Average cohesion_score:", avg_cohesion_score)
 
-    phraseology_scores = []
-    for i in range(len(sections)-1):
-        score = calculate_phrase_relation_score(sections[i], sections[i+1])
-        phraseology_scores.append(score)
-    phraseology = 0
-    if(len(phraseology_scores2)>0):
-        phraseology = sum(phraseology_scores)/len(phraseology_scores)
-    # print("Phraseology Scores for Sections:")
-    # for i, score in enumerate(phraseology_scores, 1):
-    #     print(f"Section {i}: {score}")
-    # print("Average Phraseology : ", sum(phraseology_scores)/len(phraseology_scores))
-
-
-
-    print("Vocabulary Score:" , calculate_vocabulary_score2(essay_text))
-
-
-    score = calculate_vocabulary_score(essay_text)
-    print("Vocabulary score:", score)
-
-
-    list_cohesion_score = []
-    sentences = sent_tokenize(essay_text)
-    for sentence in sentences:
-        cohesion_score = calculate_cohesion_score(sentence)
-        list_cohesion_score.append(cohesion_score)
-        # print("Cohesion score for senten ce '{}': {}".format(sentence, cohesion_score))
-
-    print("Average cohesion_score : ", sum(list_cohesion_score)/len(list_cohesion_score))
-
-
-    score = conventions_score(essay_text)
-    print("Conventions Score:", score)
+    # Calculate conventions score
+    convention_score = conventions_score(essay_text)
+    print("Conventions Score:", convention_score)
 
     print("______________________________________________________________ ")
+
+# Analyze each essay text
+for essay_text in essay_text_list:
+    analyze_essay(essay_text)
